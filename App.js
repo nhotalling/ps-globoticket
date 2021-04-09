ed84e03390d3bd818454c6e1e8bbe78847472c5d
Nick Hotalling <nick.hotalling@gmail.com>
2021-04-09 13:23:22 -0500

    Install navigation, stub in navigationContainer

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+// gesture handler should be first
+import 'react-native-gesture-handler';
 import React from 'react';
 import type {Node} from 'react';
 import {StatusBar} from 'react-native';
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation';
 import Home from './home';
+
+// all views in the app will be inside the NavigationContainer
+// StackedNavigator allows you to transition between screens of app
+// also ensures that it is consistent on both ios and android
+
+const Stack = createStackNavigator();
 
 const App: () => Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" hidden />
-      <Home username="Sports Dude"></Home>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" headerMode="screen">
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
---
+++ README.md
@@ -0,0 +1,8 @@
+
+Installed:
+
+
+npm install @react-navigation/native @react-navigation/stack
+npm install react-native-reanimated react-native-gesture-handler
+npm install react-native-screens react-native-safe-area-context
+npm install @react-native-community/masked-view
