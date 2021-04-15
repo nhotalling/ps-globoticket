353c6fa3236c09512d47ecdbf496f0869cbaf3a0
Nick Hotalling <nick.hotalling@gmail.com>
2021-04-15 12:13:33 -0500

    added source extensions to metro - unsure if needed

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,15 @@
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
  *
+ * nh: added resolver and sourceExts but it may not have been necessary
+ *
  * @format
  */
 
 module.exports = {
+  resolver: {
+    sourceExts: ['js', 'json', 'ts', 'tsx'],
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
