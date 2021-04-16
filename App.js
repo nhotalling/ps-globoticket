a5774a4fff14a827bf5b5ebdfc8a26e7fe9263dd
Nick Hotalling <nick.hotalling@gmail.com>
2021-04-16 12:00:39 -0500

    Prettier change

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import type {Node} from 'react';
-import {StatusBar} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import type { Node } from 'react';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Home from './home';
 
 // all views in the app will be inside the NavigationContainer
@@ -19,7 +19,7 @@ const App: () => Node = () => {
       <StatusBar barStyle="dark-content" hidden />
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home" headerMode="screen">
-          <Stack.Screen name="Home" options={{headerShown: false}}>
+          <Stack.Screen name="Home" options={{ headerShown: false }}>
             {props => <Home {...props} username="Sports Dude2" />}
           </Stack.Screen>
         </Stack.Navigator>
